6cc696cb5a03fe47010dea922de7e54c1c1024d6
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2022-11-17 22:55:48 +0700

    Update __init__.py

--- geezram/__init__.py
+++ geezram/__init__.py
@@ -134,89 +134,88 @@ if ENV:
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
-else:
+#else:
     #from geezram.config import Development as Config
 
-    TOKEN = Config.TOKEN
+    #TOKEN = Config.TOKEN
 
-    try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
+   # try:
+   #     OWNER_ID = int(Config.OWNER_ID)
+   # except ValueError:
+    #    raise Exception("Your OWNER_ID variable is not a valid integer.")
 
-    JOIN_LOGGER = Config.JOIN_LOGGER
-    OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
-    try:
-        DRAGONS = {int(x) for x in Config.DRAGONS or []}
-        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
+   # JOIN_LOGGER = Config.JOIN_LOGGER
+   # OWNER_USERNAME = Config.OWNER_USERNAME
+   # ALLOW_CHATS = Config.ALLOW_CHATS
+   # try:
+    #    DRAGONS = {int(x) for x in Config.DRAGONS or []}
+    #    DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
+    #except ValueError:
+      #  raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    try:
-        DEMONS = {int(x) for x in Config.DEMONS or []}
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+  #  try:
+   #     DEMONS = {int(x) for x in Config.DEMONS or []}
+   # except ValueError:
+   #     raise Exception("Your support users list does not contain valid integers.")
 
-    try:
-        WOLVES = {int(x) for x in Config.WOLVES or []}
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
+   # try:
+   #     WOLVES = {int(x) for x in Config.WOLVES or []}
+   # except ValueError:
+   #     raise Exception("Your whitelisted users list does not contain valid integers.")
 
-    try:
-        TIGERS = {int(x) for x in Config.TIGERS or []}
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+   # try:
+   #     TIGERS = {int(x) for x in Config.TIGERS or []}
+   # except ValueError:
+    #    raise Exception("Your tiger users list does not contain valid integers.")
+#
+   # EVENT_LOGS = Config.EVENT_LOGS
+  #  WEBHOOK = Config.WEBHOOK
+  #  URL = Config.URL
+  #  PORT = Config.PORT
+  #  CERT_PATH = Config.CERT_PATH
+   # API_ID = Config.API_ID
+  #  API_HASH = Config.API_HASH
 
-    EVENT_LOGS = Config.EVENT_LOGS
-    WEBHOOK = Config.WEBHOOK
-    URL = Config.URL
-    PORT = Config.PORT
-    CERT_PATH = Config.CERT_PATH
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
+  #  DB_URL = Config.DB_URL
+   # MONGO_DB_URI = Config.MONGO_DB_URI
+   # ARQ_API_KEY = Config.ARQ_API_KEY
+   # ARQ_API_URL = Config.ARQ_API_URL
+   # DONATION_LINK = Config.DONATION_LINK
+   # LOAD = Config.LOAD
+   # TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
+   # OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
+  #  NO_LOAD = Config.NO_LOAD
+   # ERROR_LOG = Config.ERROR_LOG
+   # HEROKU_API_KEY = Config.HEROKU_API_KEY
+   # HEROKU_APP_NAME = Config.HEROKU_APP_NAME
+  #  DEL_CMDS = Config.DEL_CMDS
+   # STRICT_GBAN = Config.STRICT_GBAN
+   # WORKERS = Config.WORKERS
+   # REM_BG_API_KEY = Config.REM_BG_API_KEY
+  #  BAN_STICKER = Config.BAN_STICKER
+  #  ALLOW_EXCL = Config.ALLOW_EXCL
+  #  CASH_API_KEY = Config.CASH_API_KEY
+  #  TIME_API_KEY = Config.TIME_API_KEY
+  #  WALL_API = Config.WALL_API
+  #  SUPPORT_CHAT = Config.SUPPORT_CHAT
+  #  SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
+  #  SPAMWATCH_API = Config.SPAMWATCH_API
+   # SESSION_STRING = Config.SESSION_STRING
+   # INFOPIC = Config.INFOPIC
+  #  BOT_USERNAME = Config.BOT_USERNAME
+  #  STRING_SESSION = Config.STRING_SESSION
+  #  LASTFM_API_KEY = Config.LASTFM_API_KEY
+  #  CF_API_KEY = Config.CF_API_KEY
 
-    DB_URL = Config.DB_URL
-    MONGO_DB_URI = Config.MONGO_DB_URI
-    ARQ_API_KEY = Config.ARQ_API_KEY
-    ARQ_API_URL = Config.ARQ_API_URL
-    DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
-    NO_LOAD = Config.NO_LOAD
-    ERROR_LOG = Config.ERROR_LOG
-    HEROKU_API_KEY = Config.HEROKU_API_KEY
-    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-    DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
-    WORKERS = Config.WORKERS
-    REM_BG_API_KEY = Config.REM_BG_API_KEY
-    BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
-    WALL_API = Config.WALL_API
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
-    SPAMWATCH_API = Config.SPAMWATCH_API
-    SESSION_STRING = Config.SESSION_STRING
-    INFOPIC = Config.INFOPIC
-    BOT_USERNAME = Config.BOT_USERNAME
-    STRING_SESSION = Config.STRING_SESSION
-    LASTFM_API_KEY = Config.LASTFM_API_KEY
-    CF_API_KEY = Config.CF_API_KEY
-
-    try:
-        BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+  #  try:
+  #      BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
+  #  except ValueError:
+  #      raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 # If you forking dont remove this id, just add your id. LOL...
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(5181183910)
 
 if not SPAMWATCH_API:
     sw = None
